6220a13b92300c062d36b98f99b6eaac0ae52274
Tien <tiendocom051@gmail.com>
2026-06-18 08:45:10 +0700

    Xay dựng đối tượng Sach

--- quanlythuvien/src/app.module.ts
+++ quanlythuvien/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { SachModule } from './sach/sach.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+
+      autoLoadEntities: true,
+      synchronize: true,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+
+    SachModule,
+  ],
 })
 export class AppModule {}
